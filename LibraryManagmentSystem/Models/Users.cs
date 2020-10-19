014e67f9bafa17821db01f87c48a9c47779e2999
Anusha Mamidala <anushamamidala817@gmail.com>
2020-10-19 19:14:55 -0400

    Added Repo

---
+++ LibraryManagmentSystem/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagmentSystem.Models
+{
+    public class Users
+    {
+        public int UserId { get; }
+        public string UserName { get; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public int RoleId { get;  }
+    }
+}
